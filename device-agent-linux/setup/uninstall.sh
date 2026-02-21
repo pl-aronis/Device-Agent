c5e9988f46c045951400d77bd142682f7fc228d8
tanishm <wanderwithanishm2024@gmail.com>
2026-02-21 18:25:02 +0530

    remove immutability and remove files

--- device-agent-linux/setup/uninstall.sh
+++ device-agent-linux/setup/uninstall.sh
@@ -5,13 +5,6 @@ SERVICE_FILE="/etc/systemd/system/device-agent-linux.service"
 BINARY_FILE="/usr/local/bin/device-agent-linux"
 BACKUP_DIR="/var/lib/.device-cache"
 
-# Remove binary and backup
-rm -f $BINARY_FILE
-rm -rf $BACKUP_DIR
-
-# Remove service
-rm -f $SERVICE_FILE
-
 # Reload systemd to recognize the removed service
 systemctl daemon-reload
 
@@ -22,6 +15,13 @@ systemctl disable device-agent-linux
 # Remove immutability
 chattr -i $BINARY_FILE
 chattr -i $SERVICE_FILE
+
+# Remove binary and backup
+rm -f $BINARY_FILE
+rm -rf $BACKUP_DIR
+
+# Remove service
+rm -f $SERVICE_FILE
 
 # Output success message
 echo "Device Agent uninstalled successfully."
